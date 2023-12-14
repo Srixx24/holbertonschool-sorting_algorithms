768d34035250f4b7f59f0645d6122dd686561ce4
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-13 19:02:18 -0800

    bubble

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * swap - swaping values
+ * @a: first value
+ * @b: second value
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+	
+	*a = *b;
+	*b = temp;
+}
+/**
+ * bubble_sort - Bubble sort algorithm
+ * @array: array
+ * @size: size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t x, y, z;
+
+	if (array == NULL || size == 0)
+		return;
+	for (x = 0; x < size -1 1; x++)
+	{
+		for (y = 0; y < size - x - 1; y++)
+		{
+			if (array[y] > array[y + 1])
+			{
+				swap(&array[y], &array[y + 1]);
+				printf("Result: ");
+				for (z = 0; z < size; z++)
+				{
+					printf("%d ", array[z]);
+				}
+				printf("\n");
+			}
+		}
+	}
+}
---
+++ sort.h
@@ -0,0 +1,8 @@
+#ifndef HEAD_
+#define HEAD_
+
+void bubble_sort(int *array, size_t size);
+
+void swap(int *a, int *b);
+
+#endif
