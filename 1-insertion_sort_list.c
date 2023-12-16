b50caf0ba518e266c1caa7380c74e062bd75eb62
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-15 20:24:11 -0800

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -34,7 +34,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp, *x, *prev;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	temp = (*list)->next;
