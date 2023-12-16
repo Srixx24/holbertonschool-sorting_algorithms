308022a5193515d10fda89c3ce1164cd059738b8
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-15 18:56:38 -0800

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 #include "sort.h"
 /**
- * selection_sort - 
+ * selection_sort - Selection sort algorithm
  * @array: array
  * @size: size
  */
 void selection_sort(int *array, size_t size)
 {
-	boop
+	size_t x, y, z;
+	size_t index = 0;
+	size_t temp;
+
+	if (array == NULL || size <= 1)
+		return;
+
+	for (x = 0; x < size - 1; x++)
+	{
+		index = x;
+		for (y = x + 1; y < size - 1; y++)
+		{
+			if (array[y] < array[index])
+				index = y;
+		}
+
+		if (index != x)
+		{
+			temp = array[i];
+			array[x] = array[index];
+			array[index] = temp;
+
+			for (z = 0; z <size; z++)
+				printf("%d ", array[z]);
+
+			printf("\n");
+		}
+	}
+}
